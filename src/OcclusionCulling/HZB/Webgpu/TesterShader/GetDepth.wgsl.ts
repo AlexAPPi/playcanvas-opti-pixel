c79081a56e1314986343a87a12d6e126998dfea9
Alexandr Shamarin <alexsandrshamarin@yandex.ru>
2026-05-13 10:45:24 +0300

    Fix hzb offset for get depth

--- src/OcclusionCulling/HZB/Webgpu/TesterShader/GetDepth.wgsl.ts
+++ src/OcclusionCulling/HZB/Webgpu/TesterShader/GetDepth.wgsl.ts
@@ -4,8 +4,10 @@ export default `
 
     fn getDepth(uv: vec2<f32>, lod: f32) -> f32 {
 
-        let uvMirrorY: vec2<f32> = vec2<f32>(uv.x, 1.0 - uv.y);
-        let data: vec4<f32> = textureSampleLevel(hzb, hzbSampler, uvMirrorY, lod);
+        // We sample the UV coordinates relative to the Level 0 HZB mipmap canvas,
+        // since the rendering along the Y-axis was inverted.
+        let adaptedUv: vec2<f32> = vec2<f32>(uv.x, uniforms.hzbUvFactor.y - uv.y);
+        let data: vec4<f32> = textureSampleLevel(hzb, hzbSampler, adaptedUv, lod);
 
         #ifdef (DEPTH_IS_FLOAT || DEPTH_IS_FLOAT16)
             return data.r;
