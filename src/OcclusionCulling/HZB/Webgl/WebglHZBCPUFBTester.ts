b97bf18921be4e0029ed39fd113ba1989db11aa6
Alexapp <alexsandrshamarin@yandex.ru>
2026-07-29 16:12:17 +0300

    Remove type workaround.

--- src/OcclusionCulling/HZB/Webgl/WebglHZBCPUFBTester.ts
+++ src/OcclusionCulling/HZB/Webgl/WebglHZBCPUFBTester.ts
@@ -140,7 +140,7 @@ export class WebglHZBCPUFBTester implements IHierarchicalZBufferTester, IGPU2CPU
             }
         });
 
-        const gl = (this._hzb.device as pc.WebglGraphicsDevice).gl;
+        const gl = this._hzb.device.gl;
         const glProgram = this._shader.impl.glProgram;
 
         gl.transformFeedbackVaryings(glProgram, ["out_flags"], gl.INTERLEAVED_ATTRIBS);
